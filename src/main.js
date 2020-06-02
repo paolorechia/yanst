ee42fc59dc3058fbfb835b1937602412e4633cd0
Paolo Rechia <paolo@e-bot7.com>
2020-06-02 17:02:02 -0300

    Minor fix

--- src/main.js
+++ src/main.js
@@ -16,6 +16,15 @@ const RouterVisitor = require("./routerVisitor").RouterVisitor;
 
 let symbols;
 
+
+// ES5 var
+//
+// for (var i = 0; i < LEN; i++) {
+//
+// }
+//
+// ES6 {var, let, const}
+
 const promise = new Promise(function (resolve, reject) {
   fs.readFile("./example_index.ts", "utf8", (err, data) => {
     if (err) {
@@ -33,49 +42,44 @@ const promise = new Promise(function (resolve, reject) {
     symbols = indexVisitor.visit(tree);
     const requires = Object.keys(symbols["requires"]);
 
-    let endpoints = recursiveFileReader(0, requires.length, symbols);
-    endpoints.then(
-      success => console.log(success)
-    ).catch(
-      error => console.log(error)
-    )
-    console.log(endpoints);
+    let endpoints = recursiveFileReader(0, requires.length, symbols, () =>
+      console.log(symbols)
+    );
   });
 });
 
-function recursiveFileReader(index, maxIndex, symbols) {
-  return new Promise((resolve) => {
-    if (index >= maxIndex) {
-      return resolve();
-    }
-    else {
-      const requires = Object.keys(symbols["requires"]);
-      console.log("requires", requires);
-      const key = requires[index]
-      console.log("key", key)
-      const filepath = symbols["requires"][key]["filepath"] + ".ts"
-      console.log("filepath", filepath);
-      readRouterFile(filepath).then(
-        t => {
-          console.log(index, maxIndex, symbols);
-          index++;
-          return resolve(recursiveFileReader(index, maxIndex, symbols));
-        }
-      ).catch(
-        failure => console.log(failure)
-      )
-    }
-  });
-};
+function recursiveFileReader(index, maxIndex, symbols, callback) {
+  if (index >= maxIndex) {
+    return callback();
+  } else {
+    const requires = Object.keys(symbols["requires"]);
+    console.log("requires", requires);
+    const key = requires[index];
+    console.log("key", key);
+    const filepath = symbols["requires"][key]["filepath"] + ".ts";
+    console.log("filepath", filepath);
+    readRouterFile(filepath)
+      .then((t) => {
+        console.log(index, maxIndex, symbols);
+        index++;
+        return recursiveFileReader(index, maxIndex, symbols, callback);
+      })
+      .catch((err) => {
+        console.log(err);
+        index++;
+        return recursiveFileReader(index, maxIndex, symbols, callback);
+      });
+  }
+}
 
 function readRouterFile(filepath) {
-    return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     fs.readFile(filepath, "utf8", (err, data) => {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       if (!data) {
-        reject("Could not open file " + filepath);
+        return reject("Could not open file " + filepath);
       }
       var input = data;
       const chars = new antlr4.InputStream(input);
@@ -85,9 +89,9 @@ function readRouterFile(filepath) {
 
       const tree = parser.routerfile();
       const routerVisitor = new RouterVisitor();
-      resolve(routerVisitor.visit(tree));
-      });
+      return resolve(routerVisitor.visit(tree));
     });
+  });
 }
 
 function toJSON(symbols) {
@@ -95,5 +99,3 @@ function toJSON(symbols) {
   console.log(JSON.stringify(symbols, null, 4));
   // setTimeout(() => console.log(JSON.stringify(symbols, null, 4)), 1000);
 }
-
-promise.then(toJSON(symbols));

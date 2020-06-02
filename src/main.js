c7259f264505d413fa600c03867ca8b2e9450e6c
Paolo Rechia <paolo@e-bot7.com>
2020-06-02 16:51:26 -0300

    Promise verison

--- src/main.js
+++ src/main.js
@@ -14,31 +14,68 @@ const RouterGrammarVisitor = require("./router/RouterGrammarVisitor");
 
 const RouterVisitor = require("./routerVisitor").RouterVisitor;
 
-
 let symbols;
-fs.readFile("./example_index.ts", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  let input = data;
 
-  const chars = new antlr4.InputStream(input);
-  const lexer = new IndexGrammarLexer.IndexGrammarLexer(chars);
-  const tokens = new antlr4.CommonTokenStream(lexer);
-  const parser = new IndexGrammarParser.IndexGrammarParser(tokens);
-  const tree = parser.indexfile();
-  const indexVisitor = new IndexVisitor();
-  symbols = indexVisitor.visit(tree);
-  console.log(symbols);
-  Object.keys(symbols["requires"]).forEach((key) => {
-    // console.log(key);
-    filepath = symbols["requires"][key]["filepath"] + ".ts";
-    console.log("Parsing file", filepath);
+const promise = new Promise(function (resolve, reject) {
+  fs.readFile("./example_index.ts", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let input = data;
+
+    const chars = new antlr4.InputStream(input);
+    const lexer = new IndexGrammarLexer.IndexGrammarLexer(chars);
+    const tokens = new antlr4.CommonTokenStream(lexer);
+    const parser = new IndexGrammarParser.IndexGrammarParser(tokens);
+    const tree = parser.indexfile();
+    const indexVisitor = new IndexVisitor();
+    symbols = indexVisitor.visit(tree);
+    const requires = Object.keys(symbols["requires"]);
+
+    let endpoints = recursiveFileReader(0, requires.length, symbols);
+    endpoints.then(
+      success => console.log(success)
+    ).catch(
+      error => console.log(error)
+    )
+    console.log(endpoints);
+  });
+});
+
+function recursiveFileReader(index, maxIndex, symbols) {
+  return new Promise((resolve) => {
+    if (index >= maxIndex) {
+      return resolve();
+    }
+    else {
+      const requires = Object.keys(symbols["requires"]);
+      console.log("requires", requires);
+      const key = requires[index]
+      console.log("key", key)
+      const filepath = symbols["requires"][key]["filepath"] + ".ts"
+      console.log("filepath", filepath);
+      readRouterFile(filepath).then(
+        t => {
+          console.log(index, maxIndex, symbols);
+          index++;
+          return resolve(recursiveFileReader(index, maxIndex, symbols));
+        }
+      ).catch(
+        failure => console.log(failure)
+      )
+    }
+  });
+};
+
+function readRouterFile(filepath) {
+    return new Promise(function(resolve, reject) {
     fs.readFile(filepath, "utf8", (err, data) => {
       if (err) {
-        console.error(err);
-        return;
+        reject(err);
+      }
+      if (!data) {
+        reject("Could not open file " + filepath);
       }
       var input = data;
       const chars = new antlr4.InputStream(input);
@@ -48,11 +85,15 @@ fs.readFile("./example_index.ts", "utf8", (err, data) => {
 
       const tree = parser.routerfile();
       const routerVisitor = new RouterVisitor();
-      const endpoints = routerVisitor.visit(tree);
-      symbols["requires"][key]['endpoints'] = endpoints;
-      console.log("Endpoints: ", endpoints);
+      resolve(routerVisitor.visit(tree));
+      });
     });
-  });
-});
+}
 
-setTimeout(() => console.log(symbols), 1000);
+function toJSON(symbols) {
+  console.log("Resolving promise");
+  console.log(JSON.stringify(symbols, null, 4));
+  // setTimeout(() => console.log(JSON.stringify(symbols, null, 4)), 1000);
+}
+
+promise.then(toJSON(symbols));
